c346a3b07f3959179ac6eb8c7cbc1bb9d466ce87
Boop <126187722+I3o0p@users.noreply.github.com>
2024-07-28 14:55:34 +0300

    Update 4.3.py

--- 4.3.py
+++ 4.3.py
@@ -1,15 +1,12 @@
-def create_new_list(lst):
-    if len(lst) < 3:
-        raise ValueError("Список повинен містити принаймні 3 елементи")
-    return [lst[0], lst[2], lst[-2]]
-
-
-examples = [
+lists = [
     [1, 2, 3, 4, 5, 6, 7, 9],
     [1, 1, 2, 1],
     [6, 3, 7]
 ]
 
-for example in examples:
-    new_list = create_new_list(example)
+for example in lists:
+    if len(example) < 3:
+        raise ValueError("Список повинен містити принаймні 3 елементи")
+
+    new_list = [example[0], example[2], example[-2]]
     print(f"{example} => {new_list}")
