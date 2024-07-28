9300210fb3ccec216e77e8d086586315f5018798
Boop <126187722+I3o0p@users.noreply.github.com>
2024-07-28 15:32:14 +0300

    Create 5.2.py

---
+++ 5.2.py
@@ -0,0 +1,32 @@
+while True:
+    try:
+        num1 = float(input("Введіть перше число: "))
+        operation = input("Введіть операцію (+, -, *, /): ")
+        num2 = float(input("Введіть друге число: "))
+
+        if operation == '+':
+            result = num1 + num2
+        elif operation == '-':
+            result = num1 - num2
+        elif operation == '*':
+            result = num1 * num2
+        elif operation == '/':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                print("Ділення на нуль!")
+                continue
+        else:
+            print("Неправильна операція!")
+            continue
+
+        print(f"Результат: {result}")
+
+    except ValueError:
+        print("Введено некоректне число!")
+        continue
+
+    continue_calculation = input("Бажаєте продовжити? (y/yes для продовження): ").strip().lower()
+    if continue_calculation not in ('y', 'yes'):
+        print("Робота калькулятора завершена.")
+        break
