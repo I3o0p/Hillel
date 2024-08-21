f39de1d072a7a6f4f5a3f673afa183e9f7e9f3bb
EgorL <sof1w@outlook.com>
2024-08-21 11:37:34 +0300

    19.08

--- 12.2.py
+++ 12.2.py
@@ -6,7 +6,8 @@ class Item:
         self.dimensions = dimensions
 
     def __str__(self):
-        return f'{self.name}, price: {self.price}'
+        return f"{self.name}, price: {self.price}"
+
 
 class User:
     def __init__(self, name, surname, numberphone):
@@ -15,7 +16,8 @@ class User:
         self.numberphone = numberphone
 
     def __str__(self):
-        return f'{self.name} {self.surname}'
+        return f"{self.name} {self.surname}"
+
 
 class Purchase:
     def __init__(self, user):
@@ -23,18 +25,15 @@ class Purchase:
         self.user = user
 
     def add_item(self, item, cnt):
-        if item in self.products:
-            self.products[item] += cnt
-        else:
-            self.products[item] = cnt
+        self.products[item] = cnt
 
     def get_total(self):
-        total = sum(item.price * cnt for item, cnt in self.products.items())
-        return total
+        return sum(item.price * cnt for item, cnt in self.products.items())
 
     def __str__(self):
-        items_str = "\n".join([f'{item.name}: {cnt} pcs.' for item, cnt in self.products.items()])
-        return f'User: {self.user}\nItems:\n{items_str}'
+        items_str = "\n".join([f"{item.name}: {cnt} pcs." for item, cnt in self.products.items()])
+        return f"User: {self.user}\nItems:\n{items_str}"
+
 
 lemon = Item('lemon', 5, "yellow", "small")
 apple = Item('apple', 2, "red", "middle")
@@ -44,14 +43,26 @@ buyer = User("Ivan", "Ivanov", "02628162")
 print(buyer)  # Ivan Ivanov
 
 cart = Purchase(buyer)
-cart.add_item(lemon, 4)  # 4 * 5 = 20
-cart.add_item(apple, 10)  # 10 * 2 = 20
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
 print(cart)
-"""
-User: Ivan Ivanov
-Items:
-lemon: 4 pcs.
-apple: 10 pcs.
-"""
+# """
+# User: Ivan Ivanov
+# Items:
+# lemon: 4 pcs.
+# apple: 20 pcs.
+# """
 
-assert cart.get_total() == 40, "Всього 40"
+assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
+assert cart.get_total() == 60, "Всього 60"
+assert cart.get_total() == 60, 'Повинно залишатися 60!'
+cart.add_item(apple, 10)
+print(cart)
+# """
+# User: Ivan Ivanov
+# Items:
+# lemon: 4 pcs.
+# apple: 10 pcs.
+# """
+
+assert cart.get_total() == 40
