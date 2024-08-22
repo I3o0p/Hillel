3e26f4e78c5a73a0ad674d53be13a8b97f77b857
EgorL <sof1w@outlook.com>
2024-08-22 23:52:11 +0300

    22.08

--- 3.2.py
+++ 3.2.py
@@ -1,25 +1,13 @@
-my_list1 = [12, 3, 4, 10]
-my_list2 = [1]
-my_list3 = []
-my_list4 = [12, 3, 4, 10, 8]
+lists = [
+    [12, 3, 4, 10],
+    [1],
+    [],
+    [12, 3, 4, 10, 8]
+]
 
+for lst in lists:
+    if len(lst) > 1:
+        lst.insert(0, lst.pop())
 
-last_element = my_list1.pop()
-my_list1.insert(0, last_element)
-
-
-last_element = my_list2.pop()
-my_list2.insert(0, last_element)
-
-if my_list3:
-    last_element = my_list3.pop()
-    my_list3.insert(0, last_element)
-
-
-last_element = my_list4.pop()
-my_list4.insert(0, last_element)
-
-print(my_list1)
-print(my_list2)
-print(my_list3)
-print(my_list4)
+for lst in lists:
+    print(lst)
