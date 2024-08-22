152af19db61fb30fbcf27159967b8a2c3a390f14
EgorL <sof1w@outlook.com>
2024-08-22 18:28:27 +0300

    22.08

---
+++ 14.1.py
@@ -0,0 +1,66 @@
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age} years old, {self.gender}'
+
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f'{super().__str__()}, Record Book: {self.record_book}'
+
+
+class GroupOverflowError(Exception):
+    pass
+
+
+class Group:
+    MAX_STUDENTS = 10
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= self.MAX_STUDENTS:
+            raise GroupOverflowError("Cannot add more than 10 students to the group.")
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number: {self.number}\n{all_students}'
+
+
+try:
+    st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+    st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+    gr = Group('PD1')
+
+    for i in range(1, 11):  # Додаємо 10 студентів
+        gr.add_student(Student('Gender', 20 + i, f'FirstName{i}', f'LastName{i}', f'RB{i}'))
+
+    gr.add_student(st1)
+
+except GroupOverflowError as e:
+    print(f"Error: {e}")
+
+print(gr)
