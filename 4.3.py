70615216ccbd744256abfaa07f49d07bda093414
EgorL <sof1w@outlook.com>
2024-08-22 15:53:35 +0300

    19.08

--- 4.3.py
+++ 4.3.py
@@ -1,12 +1,9 @@
-lists = [
-    [1, 2, 3, 4, 5, 6, 7, 9],
-    [1, 1, 2, 1],
-    [6, 3, 7]
-]
+list1 = [1, 2, 3, 4, 5, 6, 7, 9]
+new_list1 = [list1[0], list1[2], list1[-2]]
 
-for example in lists:
-    if len(example) < 3:
-        raise ValueError("Список повинен містити принаймні 3 елементи")
+list2 = [1, 1, 2, 1]
+new_list2 = [list2[0], list2[2], list2[-2]]
 
-    new_list = [example[0], example[2], example[-2]]
-    print(f"{example} => {new_list}")
+list3 = [6, 3, 7]
+new_list3 = [list3[0], list3[2], list3[-2]]
+print(new_list1,new_list2,new_list3)
